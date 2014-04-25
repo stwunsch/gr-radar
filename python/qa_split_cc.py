599c95495fda39ecff993c2bbc802b53023cc9f8
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2014-04-25 13:13:56 +0200

    add py testcase for correct splitting, emphase on correct data in output

--- python/qa_split_cc.py
+++ python/qa_split_cc.py
@@ -384,6 +384,22 @@ class qa_split_cc (gr_unittest.TestCase):
 		self.assertEqual(len(snk0.data()),mult*samp_cw) # check correct number of samples in every sink
 		self.assertEqual(len(snk1.data()),mult*samp_up)
 		self.assertEqual(len(snk2.data()),mult*samp_down)
+		
+	def test_002_t (self):
+		# set up fg
+		in_data = (1, 2, 3, 4, 5, 6)
+		packet_len = len(in_data)
+		
+		src = blocks.vector_source_c(in_data)
+		s2ts = blocks.stream_to_tagged_stream(8,1,packet_len,"packet_len")
+		split = radar.split_cc(1,(1,3,2))
+		snk = blocks.vector_sink_c()
+		
+		self.tb.connect(src,s2ts,split,snk)
+		self.tb.run ()
+		
+		# check data
+		self.assertTupleEqual(snk.data(),(2,3,3)) # check if correct data is pushed to output
 
 if __name__ == '__main__':
 	gr_unittest.run(qa_split_cc)#, "qa_split_cc.xml")
